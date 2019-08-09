1bdc87633e4933ccb8b6f044a3b9ccdb6f8556d8
Shoe Pac <shoepac32@gmail.com>
2019-08-09 20:02:09 +0200

    Revert "YouTravel_Live"
    This reverts commit c8c814e5275540134ad36b3cc3c27556a99c5807.

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-      <div className="footer"> 
-           Copyright © 2019 Zagreb - Codifica Ltd - All rights reserved.     
-      </div>
-);
-
-export default Footer;
